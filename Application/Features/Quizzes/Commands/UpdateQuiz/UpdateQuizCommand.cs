db77d97813f31583b2a05fce1121d94687d353a6
ibrahem nasr <ibrahem nasr@gmail.com>
2026-04-30 17:33:39 +0300

    Fix Bug For UpdateQuiz Endpoint

--- Application/Features/Quizzes/Commands/UpdateQuiz/UpdateQuizCommand.cs
+++ Application/Features/Quizzes/Commands/UpdateQuiz/UpdateQuizCommand.cs
@@ -1,10 +1,5 @@
-﻿using Application.Features.ModuleItems.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Application.Features.Quizzes.Commands.UpdateQuiz
+﻿namespace Application.Features.Quizzes.Commands.UpdateQuiz
 {
     public sealed record UpdateQuizCommand(int ModuleId, int CourseId, int ItemId, int Duration, int PassingScore, bool ShowCorrectAnswers, bool ShuffleQuestions, TimeOnly StartTime,
-         TimeOnly EndTime, DateOnly StartDate, DateOnly EndDate) : IRequest<OneOf<SuccessDto, Error>>;
+         TimeOnly EndTime, DateTime StartDate, DateTime EndDate) : IRequest<OneOf<SuccessDto, Error>>;
 }
