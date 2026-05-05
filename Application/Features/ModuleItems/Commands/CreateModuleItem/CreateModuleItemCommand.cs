faddf13744b012da0477056cc64c3619a7f08313
ibrahem nasr <ibrahem nasr@gmail.com>
2026-05-05 17:38:18 +0300

    Fix Bug For CreateModuleItem Endpoint

--- Application/Features/ModuleItems/Commands/CreateModuleItem/CreateModuleItemCommand.cs
+++ Application/Features/ModuleItems/Commands/CreateModuleItem/CreateModuleItemCommand.cs
@@ -5,7 +5,7 @@ namespace Application.Features.ModuleItems.Commands.CreateModuleItem
 {
     public sealed record CreateModuleItemCommand(int CourseId, int ModuleId, ModuleItemType Type, string Title, CourseStatus Status, bool AllowDiscussions,
         IEnumerable<ConditionDto> Conditions, string? Description, string? VideoId, IEnumerable<Resource>? Resources,
-        DateTime? DueDate, string? Instructions, int? TotalMarks, SubmissionType? SubmisssionType,
+        DateTime? DueDate, string? Instructions, int? TotalMarks, SubmissionType? SubmissionType,
         IEnumerable<Attachment>? Attachments, int? Duration, int? PassingScore, bool? ShowCorrectAnswers, bool? ShuffleQuestions,
         DateTime? StartDate, DateTime? EndDate, TimeOnly? StartTime, TimeOnly? EndTime) : IRequest<OneOf<SuccessDto, Error>>;
 }
