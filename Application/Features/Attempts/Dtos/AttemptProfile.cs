31feaec7e79255db3ba5138ef7a4769666a7b07c
ibrahem nasr <ibrahem nasr@gmail.com>
2026-05-07 18:11:58 +0300

    Fix Bug

--- Application/Features/Attempts/Dtos/AttemptProfile.cs
+++ Application/Features/Attempts/Dtos/AttemptProfile.cs
@@ -5,36 +5,36 @@
         public AttemptProfile()
         {
             CreateMap<QuizAttempt, SummaryDto>()
-                .ForMember(dest => dest.Correct, src => src.MapFrom(a => a.Answers.Count(ans => ans.IsCorrect)))
-                .ForMember(dest => dest.Wrong, src => src.MapFrom(a => a.Answers.Count(ans => !ans.IsCorrect && ans.StudentAnswer != null)))
-                .ForMember(dest => dest.Skipped, src => src.MapFrom(a => a.Answers.Count(ans => ans.StudentAnswer == null)));
-            
+                .ForMember(dest => dest.Correct, opt => opt.MapFrom(src => src.Answers.Count(ans => ans.IsCorrect)))
+                .ForMember(dest => dest.Wrong, opt => opt.MapFrom(src => src.Answers.Count(ans => !ans.IsCorrect && ans.StudentAnswer != null)))
+                .ForMember(dest => dest.Skipped, opt => opt.MapFrom(src => src.Answers.Count(ans => ans.StudentAnswer == null)));
+
             CreateMap<Student, StudentDto>()
-                .ForMember(dest => dest.Id, src => src.MapFrom(s => s.Id))
-                .ForMember(dest => dest.Name, src => src.MapFrom(s => s.User.FirstName + " " + s.User.LastName))
-                .ForMember(dest => dest.ProfilePicture, src => src.MapFrom(s => s.User.ProfilePicture));
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.FirstName + " " + src.User.LastName))
+                .ForMember(dest => dest.ProfilePicture, opt => opt.MapFrom(src => src.User.ProfilePicture));
 
             CreateMap<QuizAttempt, AttemptResponse>()
-                .ForMember(dest => dest.QuestionCount, src => src.MapFrom(a => a.Answers.Count))
-                .ForMember(dest => dest.Questions, src => src.MapFrom(a => a.Answers))
+                .ForMember(dest => dest.QuestionCount, opt => opt.MapFrom(src => src.Answers.Count))
+                .ForMember(dest => dest.Questions, opt => opt.MapFrom(src => src.Answers))
                 .ForMember(dest => dest.Summary, opt => opt.MapFrom(src => src));
-            
+
             CreateMap<Answer, QuestionAttempt>()
-                .ForMember(dest => dest.QuestionId, src => src.MapFrom(a => a.QuizQuestionId))
-                .ForMember(dest => dest.Question, src => src.MapFrom(a => a.QuizQuestion.Question.QuestionTitle))
-                .ForMember(dest => dest.Options, src => src.MapFrom(a => a.QuizQuestion.Question.Options))
-                .ForMember(dest => dest.CorrectAnswer, src => src.MapFrom(a => a.QuizQuestion.Question.CorrectAnswer))
-                .ForMember(dest => dest.Explanation, src => src.MapFrom(a => a.QuizQuestion.Question.Explanation))
-                .ForMember(dest => dest.Marks, src => src.MapFrom(a => a.QuizQuestion.Marks))
-                .ForMember(dest => dest.Type, src => src.MapFrom(a => a.QuizQuestion.Question.Type))
-                .ForMember(dest => dest.Order, src => src.MapFrom(a => a.QuizQuestion.Order))
-                .ForMember(dest => dest.Answer, src => src.MapFrom(a => new AnswerDto
+                .ForMember(dest => dest.QuestionId, opt => opt.MapFrom(src => src.QuizQuestionId))
+                .ForMember(dest => dest.Question, opt => opt.MapFrom(src => src.QuizQuestion.Question.QuestionTitle))
+                .ForMember(dest => dest.Options, opt => opt.MapFrom(src => src.QuizQuestion.Question.Options))
+                .ForMember(dest => dest.CorrectAnswer, opt => opt.MapFrom(src => src.QuizQuestion.Question.CorrectAnswer))
+                .ForMember(dest => dest.Explanation, opt => opt.MapFrom(src => src.QuizQuestion.Question.Explanation))
+                .ForMember(dest => dest.Marks, opt => opt.MapFrom(src => src.QuizQuestion.Marks))
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.QuizQuestion.Question.Type))
+                .ForMember(dest => dest.Order, opt => opt.MapFrom(src => src.QuizQuestion.Order))
+                .ForMember(dest => dest.Answer, opt => opt.MapFrom(src => new AnswerDto
                 {
-                    Value = a.StudentAnswer,
-                    IsCorrect = a.IsCorrect,
-                    AutoScore = a.AutoScore,
-                    TeacherScore = a.TeacherScore,
-                    Feedback = a.Feedback,
+                    Value = src.StudentAnswer,
+                    IsCorrect = src.IsCorrect,
+                    AutoScore = src.AutoScore,
+                    TeacherScore = src.TeacherScore,
+                    Feedback = src.Feedback,
                 }));
         }
     }
