7b3489d76baf296f5ca094d7b576a3a8a18b4a49
ibrahem nasr <ibrahem nasr@gmail.com>
2026-05-14 20:57:25 +0300

    Fix Bug For CreateManualGrade Endpoint

--- Application/Features/Attempts/Commands/CreateAttemptManualGrading/CreateAttemptManualGradeCommand.cs
+++ Application/Features/Attempts/Commands/CreateAttemptManualGrading/CreateAttemptManualGradeCommand.cs
@@ -3,6 +3,6 @@ using Domain.Enums;
 
 namespace Application.Features.Attempts.Commands.CreateAttemptManualGrading
 {
-    public sealed record CreateAttemptManualGradeCommand(int QuizId, int AttemptId, List<QuestionDto> Questions, double OverallScore, GradingStatus Status)
-        : IRequest<OneOf<bool, Error>>;
+    public sealed record CreateAttemptManualGradeCommand(int QuizId, int AttemptId, List<QuestionDto> Questions, double OverallScore,
+        GradingStatus Status) : IRequest<OneOf<bool, Error>>;
 }
--- Application/Features/Attempts/Commands/CreateAttemptManualGrading/CreateAttemptManualGradeCommandHandler.cs
+++ Application/Features/Attempts/Commands/CreateAttemptManualGrading/CreateAttemptManualGradeCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Features.Attempts.Commands.CreateAttemptManualGrading
 {
@@ -11,8 +12,12 @@ namespace Application.Features.Attempts.Commands.CreateAttemptManualGrading
         private readonly IAnswerRepository _answerRepository;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IStudentGradeRepository _studentGradeRepository;
+        private readonly ITenantRepository _tenantRepository;
+
         public CreateAttemptManualGradeCommandHandler(ITenantMemberRepository tenantMemberRepository, ICurrentUserId currentUserId, ISubscriptionRepository subscriptionRepository,
-            IAttemptRepository attemptRepository, IAnswerRepository answerRepository, IHttpContextAccessor httpContextAccessor, IUnitOfWork unitOfWork)
+            IAttemptRepository attemptRepository, IAnswerRepository answerRepository, IHttpContextAccessor httpContextAccessor, 
+            IUnitOfWork unitOfWork, IStudentGradeRepository studentGradeRepository, ITenantRepository tenantRepository)
         {
             _tenantMemberRepository = tenantMemberRepository;
             _currentUserId = currentUserId;
@@ -21,6 +26,8 @@ namespace Application.Features.Attempts.Commands.CreateAttemptManualGrading
             _answerRepository = answerRepository;
             _httpContextAccessor = httpContextAccessor;
             _unitOfWork = unitOfWork;
+            _studentGradeRepository = studentGradeRepository;
+            _tenantRepository = tenantRepository;
         }
         public async Task<OneOf<bool, Error>> Handle(CreateAttemptManualGradeCommand request, CancellationToken cancellationToken)
         {
@@ -28,25 +35,36 @@ namespace Application.Features.Attempts.Commands.CreateAttemptManualGrading
             var subdomain = _httpContextAccessor?.HttpContext?.Request.Cookies[AuthConstants.SubDomain];
             var isPermitted = await _tenantMemberRepository.IsPermittedMember(userId, PermissionConstants.MANAGE_ATTEMPTS, cancellationToken);
             if (!isPermitted)
-            {
                 return MemberErrors.NotAllowed;
-            }
+            
             var isSubscribed = await _subscriptionRepository.HasActiveSubscriptionByTenantDomain(subdomain!, cancellationToken);
             if (!isSubscribed)
-            {
                 return TenantErrors.NotSubscribed;
-            }
+            
             var attempt = await _attemptRepository.GetAttemptByIdAsync(request.AttemptId, request.QuizId, subdomain!, cancellationToken);
             if (attempt is null)
-            {
                 return AttemptErrors.AttemptNotFound;
-            }
+            
+            var tenantId = await _tenantRepository.GetTenantIdAsync(subdomain!, cancellationToken);
+            var graderId = await _tenantMemberRepository.GetTenantmemberIdAsync(tenantId, cancellationToken);
             await _unitOfWork.BeginTransactionAsync(cancellationToken);
             try
             {
                 await _answerRepository.UpdateTeacherScore(request.AttemptId, request.Questions, cancellationToken);
                 attempt.Score = request.OverallScore;
                 attempt.GradingStatus = request.Status;
+
+                var newStudentGrade = new StudentGrade
+                {
+                    Score = request.OverallScore,
+                    TotalMarks = attempt.TotalMarks,
+                    Type = StudentGradeType.Quiz,
+                    GraderId = graderId,
+                    StudentId = attempt.StudentId,
+                    TypeId = request.QuizId,
+                    TenantId = tenantId
+                };
+                await _studentGradeRepository.CreateStudentGradeAsync(newStudentGrade, cancellationToken);
                 await _unitOfWork.CommitTransactionAsync(cancellationToken);
                 return true;
             }
