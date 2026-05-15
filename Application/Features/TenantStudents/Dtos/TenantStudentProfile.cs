20025fefdf0e713f749894f02effcf45fd48bc18
ibrahem nasr <ibrahem nasr@gmail.com>
2026-05-15 16:32:32 +0300

    Fix Bug For GetStudentById Endpoint

--- Application/Features/TenantStudents/Dtos/TenantStudentProfile.cs
+++ Application/Features/TenantStudents/Dtos/TenantStudentProfile.cs
@@ -23,7 +23,7 @@ namespace Application.Features.TenantStudents.Dtos
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
                 .ForMember(dest => dest.ProfilePicture, opt => opt.MapFrom(src => src.User.ProfilePicture))
-                .ForMember(dest => dest.AverageGrades, opt => opt.MapFrom(src => src.StudentGrades.Any() ? (int)src.StudentGrades.Average(sg => sg.Score) : 0))
+                .ForMember(dest => dest.AverageGrades, opt => opt.MapFrom(src => src.StudentGrades.Any() ? (int)Math.Round(src.StudentGrades.Average(sg => sg.Score / sg.TotalMarks * 100)) : 0))
                 .ForMember(dest => dest.EnrolledCourses, opt => opt.MapFrom(src => src.Enrollments.Select(e => e.CourseId).ToList()))
                 .ForMember(dest => dest.Flags, opt => opt.MapFrom(src => src.StudentSubscriptions))
                 .ForMember(dest => dest.Courses, opt => opt.MapFrom(src => src.Enrollments));
