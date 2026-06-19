c2d49efd1d9adc5aed80898ef81d8c85d1dd8065
ibrahem nasr <ibrahem nasr@gmail.com>
2026-06-19 18:09:59 +0300

    Fix Bugs

--- Application/Features/Website/Dtos/WebsiteProfile.cs
+++ Application/Features/Website/Dtos/WebsiteProfile.cs
@@ -1,9 +1,52 @@
-﻿namespace Application.Features.Website.Dtos
+﻿using Application.Features.Public.Dtos;
+using Domain.Enums;
+
+namespace Application.Features.Website.Dtos
 {
-    public sealed class TenantWebsiteSettingsProfile : Profile
+    public sealed class WebsiteProfile : Profile
     {
-        public TenantWebsiteSettingsProfile()
+        public WebsiteProfile()
         {
+            CreateMap<TenantPage, TenantPagesDto>();
+
+            CreateMap<BlockType, BlockTypeDto>();
+
+            CreateMap<PageBlock, TenantBlockTypeDto>()
+                .ForMember(dest => dest.BlockType, opt => opt.MapFrom(src => src.BlockTypeId))
+                .ForMember(dest => dest.Schema, opt => opt.MapFrom(src => src.BlockType.Schema.RootElement))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.BlockType.Description))
+                .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.BlockType.DisplayName));
+
+            CreateMap<TenantMember, InstructorDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.DisplayName))
+                .ForMember(dest => dest.ProfilePicture, opt => opt.MapFrom(src => src.User.ProfilePicture));
+
+            CreateMap<Course, TenantCourseDto>()
+                .ForMember(dest => dest.IsPublished, opt => opt.MapFrom(src => src.CourseStatus == CourseStatus.Published))
+                .ForMember(dest => dest.Subject, opt => opt.MapFrom(src => src.Subject.Label))
+                .ForMember(dest => dest.Grade, opt => opt.MapFrom(src => src.Grade.Label))
+                .ForMember(dest => dest.StudentsCount, opt => opt.MapFrom(src => src.Enrollments.Count))
+                .ForMember(dest => dest.Instructor, opt => opt.MapFrom(src => src.Tenant.TenantMembers.FirstOrDefault()));
+
+            CreateMap<TenantPage, TenantNavigationLinkDto>();
+
+            CreateMap<Student, StudentDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.User.PhoneNumber))
+                .ForMember(dest => dest.ProfilePicture, opt => opt.MapFrom(src => src.User.ProfilePicture));
+
+            CreateMap<Course, CourseDto>()
+                .ForMember(dest => dest.PricingType, opt => opt.MapFrom(src => src.PricingType.ToString()))
+                .ForMember(dest => dest.BillingCycle, opt => opt.MapFrom(src => src.BillingCycle.HasValue ? src.BillingCycle.Value.ToString() : null))
+                .ForMember(dest => dest.Semester, opt => opt.MapFrom(src => src.Semester));
+
+            CreateMap<Order, TenantOrderDto>()
+                .ForMember(dest => dest.PaymentMethod, opt => opt.MapFrom(src => src.PaymentType))
+                .ForMember(dest => dest.Timeline, opt => opt.MapFrom(src => src.OrderTimeLines));
+
+            CreateMap<OrderTimeLine, TimelineDto>();
+
             // Read
             CreateMap<Tenant, TenantWebsiteSettingsDto>()
                 .ForMember(dest => dest.General, opt => opt.MapFrom(src => src))
@@ -27,7 +70,6 @@
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src));
 
             CreateMap<NotificationSetting, EmailSettingsDto>();
-
 
             // Updated
             CreateMap<GeneralDto, WebsiteSetting>()
@@ -61,6 +103,8 @@
                 .ForMember(dest => dest.Tenant, opt => opt.Ignore())
                 .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
                 .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<PaymentMethod, PaymentMethodDto>();
         }
     }
 }
